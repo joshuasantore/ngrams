856302854b6b88a417a064752172414f581cd0ed
joshuasantore <santore.joshua@gmail.com>
2025-04-24 14:13:46 -0400

    fixed README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-1. add a small corpus on line 5 of run.py
-2. specify n-gram length(1, 2, 3, etc) on line 6 of run.py
+1. add a small corpus of sentences on line 7 of run.py
+2. specify n-gram length(1, 2, 3, etc) on line 8 of run.py (very slow for anything above 3 at 300 words ish)
 2. run run.py
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from ngrams.tokenizer import tokenizer
 from ngrams.ngrams import gen_ngrams, gen_ngram_probs
 from ngrams.utils.count import get_counts
 from ngrams.sample import sample
+
 def run_app():
 	corpus = '''Mathematical logic is a branch of math that focuses on understanding how reasoning and proof work in a precise and structured way. Unlike everyday logic, which relies on intuition, it uses strict rules to analyze how statements connect, how truth is determined, and how valid conclusions can be drawn from given facts. One key area is propositional logic, which studies simple statements combined with words like and, or, and not, while predicate logic extends this by using variables and quantifiers to express more complex ideas. Another important concept is formal systems, which provide rules for constructing proofs: step-by-step arguments that show whether a statement is true or false. A major discovery in this field is Gödel’s incompleteness theorems, which prove that even the strongest logical systems cannot prove every true statement, revealing fundamental limits in math. Beyond theory, mathematical logic has practical uses in computer science, helping design programming languages, algorithms, and artificial intelligence by ensuring clear and correct reasoning. It also influences philosophy by providing tools to analyze arguments and explore truth. By studying proofs, logical systems, and computability, mathematical logic strengthens our ability to think clearly, avoid errors, and build reliable theories. Its ideas connect abstract math to real-world problems, making it essential not just for mathematicians but also for computer scientists, philosophers, and anyone interested in structured thinking.'''
 	n = 1
@@ -47,7 +48,6 @@ def run_app():
 		sentence = sample(n, probs)
 		print(sentence)
 		print()
-	
 	
 if __name__ == "__main__":
     run_app()
